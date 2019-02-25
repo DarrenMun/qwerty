5d2c8ea4e8bca1cf59652380a5e55ddadbf0794b
DarrenMun <darren@kineticskunk.com>
2019-02-25 14:48:36 +0200

    database info

--- src/darren_test/db.py
+++ src/darren_test/db.py
@@ -5,14 +5,14 @@ import os
 db.create_all()
 
 # Data to initialize database with
-# Data = [
-# 	{'name'		: 'Darren'	,'physics': 80, 'maths': 60, 'chemistry':45},
-# 	{'name'		: 'Jerry'	,'physics': 50, 'maths': 45, 'chemistry':45},
-# ]
+Data = [
+	{'name'		: 'Darren'	,'physics': 80, 'maths': 60, 'chemistry':45},
+	{'name'		: 'Jerry'	,'physics': 50, 'maths': 45, 'chemistry':45},
+]
 
-# # Iterate over the PEOPLE structure and populate the database
-# for xData in Data:
-# 	d = Student(name=xData['name'], physics=xData['physics'],maths=xData['maths'], chemistry=xData['chemistry'])
-# 	db.session.add(d)
+# Iterate over the PEOPLE structure and populate the database
+for xData in Data:
+	d = Student(name=xData['name'], physics=xData['physics'],maths=xData['maths'], chemistry=xData['chemistry'])
+	db.session.add(d)
 
 db.session.commit()
